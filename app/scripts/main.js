34563f1a7542dec17a6ab7a45f424fcb214e0a08
Judson Mitchell <judsonmitchell@gmail.com>
2015-04-06 19:19:32 -0500

    Add basic, routes, auth, and video placeholder

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,137 @@
-console.log('\'Allo \'Allo!');
+//Login User
+var ref = new Firebase('https://dazzling-torch-5906.firebaseio.com'),
+stream, 
+checkAuth = function () {
+    var authData = ref.getAuth();
+    if (authData){
+        console.log('logged in user');
+        return true;
+    } else {
+        return false;
+    }
+},
+
+loginUser = function (user,pwd) {
+    ref.authWithPassword({
+        email    : user,
+        password : pwd
+    }, function(error, authData) {
+        if (error) {
+            console.log('Login Failed!', error);
+            toastr.error('Sorry! Bad email or password.');
+        } else {
+            console.log('Authenticated successfully with payload:', authData);
+            if (!$('#remember').prop('checked')){
+                remember: 'sessionOnly'
+            }
+            //$('.content').html(Handlebars.templates.index);
+            location.href = '#/main/index';
+        }
+    });
+
+},
+
+logoutUser = function () {
+    ref.unauth();
+    //$('.content').html(Handlebars.templates.login);
+    location.href = '#/login';
+},
+
+setNav = function (index) {
+    $('ul.nav li').removeClass('active');
+    $('ul.nav li').eq(index).addClass('active');
+},
+
+startVideo = function () {
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+    navigator.getUserMedia({video:true,audio:true}, 
+        function(localMediaStream){
+                stream = localMediaStream;
+                var video = document.querySelector('video');
+                video.src = window.URL.createObjectURL(localMediaStream);
+                video.onloadedmetadata = function(e) {
+                    video.play();
+                };
+            }, 
+        function (e){
+            console.log('error'); 
+        });
+
+},
+
+stopVideo = function () {
+    stream.stop();
+
+}
+
+Path.before('#/main/', function () {
+    var authData = ref.getAuth();
+    if (authData){
+        console.log('logged in user');
+        return true;
+    } else {
+        return false;
+    }
+});
+
+Path.map('#/main/index').to(function() {
+    $('.content').html(Handlebars.templates.index);
+})
+.enter(function() {
+    $('#logout').show();
+    setNav(0)
+});
+
+Path.map('#/main/desk').to(function() {
+    $('.content').html(Handlebars.templates.desk);
+});
+
+Path.map('#/main/target').to(function() {
+    var context = {name:'Jady', org_name:'LoyolaLawClinic' };
+    $('.content').html(Handlebars.templates.target(context));
+    startVideo();
+});
+
+Path.map('#/login').to(function() {
+    $('.content').html(Handlebars.templates.login);
+})
+.enter(function () {
+    $('#logout').hide();
+    setNav(0);
+});
+
+Path.map('#/logout').to(function() {
+    logoutUser();
+});
+
+Path.map('#/about').to(function() {
+    $('.content').html(Handlebars.templates.about);
+})
+.enter(function(){
+    setNav(1);
+});
+
+Path.map('#/contact').to(function() {
+    $('.content').html(Handlebars.templates.contact);
+})
+.enter(function(){
+    setNav(2);
+});
+Path.root('#/login');
+
+Path.rescue(function () {
+    //$('.content').html(Handlebars.templates.login);
+    location.href = '#/login';
+});
+
+Path.listen();
+
+$('.content').on('submit', '#login', function(e) {
+    e. preventDefault();
+    loginUser($('#inputEmail3').val(), $('#inputPassword3').val());
+});
+
+$('.content').on('click', '.video-stop', function(e) {
+    console.log('clicked stop video');
+    stopVideo();
+});
